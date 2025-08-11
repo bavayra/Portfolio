5d0cf41fbb2c796130da5021ab55cd082ddfa067
bavayra <x.bavayra@gmail.com>
2025-08-11 17:03:47 +0200

    JS added, not working yet

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,5 +1,70 @@
-/*const calc = document.querySelector(".calcContainer");
+const display = document.getElementById("display");
+let previousNum = "";
+let currentNum = "";
+let operator = ""; 
 
-calc.addEventListener("mouseover", () => {
-    calc.classList.add("active");
-});
+function pressedNum(num){
+    currentNum += num;
+    display.textContent = currentNum;
+}
+
+function pressedOperator(op){
+    if (currentNum = ""){
+        return;
+    }
+    previousNum = currentNum;
+    operator = op;
+    currentNum ="";
+}
+
+function calculate(){
+    let result;
+    const prev = parseFloat(previousNum);
+    const curr = parseFloat(currentNum); //I used parseFloat() because it allows us to work with decimals unlike Number()
+
+    if (isNaN(prev) || isNaN(curr)){
+        return;
+    }
+
+    switch (operator){
+        case '+':
+            result = prev + curr;
+            break;
+        case '-':
+            result = prev - curr;
+            break;
+        case '*':
+            result = prev * curr;
+            break;
+        case '/': 
+            if(curr === 0){
+                display.textContent = "You cannot divide by 0";
+                display.style.fontSize = "1.1rem";
+                return;
+            }
+                result = prev / curr;
+                break;
+
+            default: return;
+    }
+
+    display.textContent = String(result);
+    currentNum = String(result);
+    previousNum = "";
+    operator = "";
+    console.log(result);
+};
+
+
+function clearResult(){
+    display.textContent = ""; 
+    currentNum = "";
+    previousNum = "";
+    operator = "";
+};
+
+function deleteLastInput(){
+    currentNum = currentNum.slice(0, -1);
+    display.textContent = currentNum;
+}
+
