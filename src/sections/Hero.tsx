14f4252142524a25defa3f4fed7a300e2cd6e21d
Alix Bell <x.bavayra@gmail.com>
2026-02-19 16:40:27 +0100

    refactor/style(Hero): increased font size and decreased font weight for h1, changed its color. Increased font size and font weight for the name text, changed its color. Increased font size for the year and changed its color. Set layout to grid, added arbitrary template, positioned elements according to draft. Decreased py of the section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -2,10 +2,16 @@ import DotPattern from "../components/DotPattern";
 
 const Hero = () => {
   return (
-    <section id="hero" className="py-20 text-center app-fullscreen">
-      <p>2026</p>
-      <h1 className="text-4xl font-extrabold">Portfolio</h1>
-      <p className="mt-4 text-gray-600">ALIX BELL</p>
+    <section id="hero" className="py-8 app-fullscreen">
+      <div className="grid  [grid-cols:200px_1fr_1fr] [grid-rows:auto_240px_auto]">
+        <p className="text-xl text-grey-medium col-start-1 row-start-1">2026</p>
+        <h1 className="text-5xl font-semibold text-grey-medium col-start-1 col-span-3 row-start-2">
+          Portfolio
+        </h1>
+        <p className=" text-accent-red font-semibold text-xl col-start-3 row-start-3">
+          ALIX BELL
+        </p>
+      </div>
       <DotPattern />
     </section>
   );
