8ce36effdc7950b1016d496b35070c4c8be44819
Alix Bell <x.bavayra@gmail.com>
2026-02-19 14:20:49 +0100

    feat: added Suspense to allow lazy rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import Header from "./sections/Header.tsx";
 import Hero from "./sections/Hero.tsx";
 import SectionTitle from "./sections/SectionTitle.tsx";
-import About from "./sections/About.tsx";
-import Projects from "./sections/Projects.tsx";
-import Contacts from "./sections/Contacts.tsx";
-import Footer from "./sections/Footer.tsx";
+import { lazy, Suspense } from "react";
+
+const About = lazy(() => import("./sections/About.tsx"));
+const Projects = lazy(() => import("./sections/Projects.tsx"));
+const Contacts = lazy(() => import("./sections/Contacts.tsx"));
+const Footer = lazy(() => import("./sections/Footer.tsx"));
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <SectionTitle />
+      <Suspense fallback={<div>Loading...</div>}>
+        <About />
+        <SectionTitle />
+        <Projects />
+        <SectionTitle />
+        <Contacts />
+        <Footer />
+      </Suspense>
+    </div>
+  );
+}
+
+export default App;
