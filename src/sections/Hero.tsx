4d23070491e13963ed2ea9d323602515a659c674
Alix Bell <x.bavayra@gmail.com>
2026-02-19 18:26:46 +0100

    style/refactor(Divider/Hero): added divider component to the Hero and positioned it correctly. Deleted 1px size prop from the Divider. Decreased font size for the year and name elements. Set pl for the year and h1, set pr for the name element

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,14 +1,24 @@
 import DotPattern from "../components/DotPattern";
+import { Divider } from "../components/Divider";
 
 const Hero = () => {
   return (
     <section id="hero" className="py-8 app-fullscreen">
-      <div className="grid  [grid-cols:200px_1fr_1fr] [grid-rows:auto_240px_auto]">
-        <p className="text-xl text-grey-medium col-start-1 row-start-1">2026</p>
-        <h1 className="text-5xl font-semibold text-grey-medium col-start-1 col-span-3 row-start-2">
+      <Divider
+        widthPx={2}
+        heightPx={88}
+        leftPx={20}
+        color="grey-medium"
+        topPx={112}
+      />
+      <div className="grid [grid-cols:200px_1fr_1fr] [grid-rows:auto_240px_auto]">
+        <p className="text-md text-grey-medium col-start-1 row-start-1 pl-1">
+          2026
+        </p>
+        <h1 className="text-5xl font-semibold text-grey-medium col-start-1 col-span-3 row-start-2 pl-1">
           Portfolio
         </h1>
-        <p className=" text-accent-red font-semibold text-xl col-start-3 row-start-3">
+        <p className=" text-accent-red font-semibold text-md col-start-3 row-start-3 pr-12">
           ALIX BELL
         </p>
       </div>
