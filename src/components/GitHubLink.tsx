65577189cbacabf9cd0216db278c8a6eb0784971
Alix Bell <x.bavayra@gmail.com>
2026-02-21 18:17:48 +0100

    refactor(GitHubLink): added an actual link to the author's gitHub profile

--- src/components/GitHubLink.tsx
+++ src/components/GitHubLink.tsx
@@ -3,7 +3,7 @@ const GitHubLink = () => {
     <div className="flex flex-row mt-4 items-center gap-1 ml-3 tracking-wide">
       <p className="text-grey-extra-dark  text-sm">Check the code on</p>
       <a
-        href="https://github.com/your-username"
+        href="https://github.com/bavayra"
         target="_blank"
         rel="noopener noreferrer"
         className="text-blue-500 hover:underline"
