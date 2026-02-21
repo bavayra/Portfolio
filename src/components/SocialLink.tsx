af36e3af8d6f7a0f9d79af87db5082d8ae693950
Alix Bell <x.bavayra@gmail.com>
2026-02-21 19:06:43 +0100

    feat: added SocialLinks component and Icon wrapper for social links svg icons.

---
+++ src/components/SocialLink.tsx
@@ -0,0 +1,80 @@
+import DiscordIcon from "/Icons/DiscordIcon.svg";
+import GitHubIcon from "/Icons/GitHubIcon.svg";
+import GmailIcon from "/Icons/GmailIcon.svg";
+import InstagramIcon from "/Icons/InstagramIcon.svg";
+import LinkedInIcon from "/Icons/LinkedInIcon.svg";
+import TelegramIcon from "/Icons/TelegramIcon.svg";
+
+import Icon from "./Icon";
+
+interface SocialLinksProps {
+  size?: "sm" | "md";
+  className?: string;
+  iconColor?: string;
+}
+
+const SocialLinks = ({
+  size = "md",
+  className,
+  iconColor = "text-accent-red",
+}: SocialLinksProps) => {
+  const Links = {
+    discord: "https://www.discord.com/bavayra",
+    github: "https://www.github.com/bavayra",
+    telegram: "https://web.telegram.org/bavayra",
+    gmail: "https://mail.google.com/mail/u/0/#inbox",
+    instagram: "https://www.instagram.com/bavayra",
+    linkedin: "https://www.linkedin.com/in/bavayra",
+  };
+  const socialNetworks = [
+    { name: "Discord", icon: DiscordIcon, url: Links.discord },
+    { name: "GitHub", icon: GitHubIcon, url: Links.github },
+    { name: "Telegram", icon: TelegramIcon, url: Links.telegram },
+    { name: "Gmail", icon: GmailIcon, url: Links.gmail },
+    { name: "Instagram", icon: InstagramIcon, url: Links.instagram },
+    { name: "LinkedIn", icon: LinkedInIcon, url: Links.linkedin },
+  ];
+
+  const sizeClasses = {
+    sm: "w-10 h-10",
+    md: "w-12  h-12",
+  };
+
+  const iconClasses = [
+    iconColor,
+    " w-10 h-10",
+    size === "md"
+      ? "border-1 border-primary-100 lg:w-6 lg:h-6"
+      : "border-primary-500 border-2",
+    "rounded-full",
+  ].join(" ");
+
+  const iconSizeMap = {
+    sm: "sm" as const,
+    md: "md" as const,
+  };
+
+  return (
+    <div className={` flex items-center justify-center ${className || ""}`}>
+      {socialNetworks.map((social) => (
+        <a
+          key={social.name}
+          href={social.url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={`${sizeClasses[size]} shrink-0 items-center justify-center rounded-full transition-transform hover:scale-110`}
+          aria-label={social.name}
+        >
+          <Icon
+            as={social.icon}
+            size={iconSizeMap[size]}
+            className={`${iconColor} ${iconClasses}`}
+            aria-hidden="true"
+          />
+        </a>
+      ))}
+    </div>
+  );
+};
+
+export default SocialLinks;
