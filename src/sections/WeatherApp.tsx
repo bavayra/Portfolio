85c0e740909fa7694ac20c0e4b98f092a87caef1
Alix Bell <x.bavayra@gmail.com>
2026-02-21 18:10:27 +0100

    feat(WeatherApp): created a WeatherApp section describing 1st project and reused GitHubLink component

--- src/sections/WeatherApp.tsx
+++ src/sections/WeatherApp.tsx
@@ -1,27 +1,26 @@
 import { Divider } from "../components/Divider";
+import GitHubLink from "../components/GitHubLink";
 
 export const WeatherApp = () => {
   return (
-    <section className="relative h-144 py-12 ml-3">
+    <section className="relative h-auto py-12 ml-3">
       <Divider
         widthPx={2}
-        heightPx={420}
+        heightPx={272}
         leftPx={0}
         color="grey-medium"
         topPx={116}
       />
-      <p className="text-grey-dark">03</p>
-      <h2 className="text-2xl text-grey-medium font-medium">
-        WHAT IS SHE LIKE
-      </h2>
+      <p className="text-grey-dark">04</p>
+      <h2 className="text-2xl text-grey-medium font-medium">WEATHER APP</h2>
       <p className="mt-2 text-xs text-grey-dark leading-loose tracking-wide ml-3 w-full max-w-[15rem] break-words whitespace-normal">
-        I'm a curious and disciplined developer who enjoys solving UI problems
-        with clean, maintainable code. I prioritize performance and
-        accessibility, and I'm committed to continuous learning. I work well in
-        teams, bringing empathy and adaptability to collaborations. I'm
-        detail-oriented and strive for perfection, learn quickly, and stay
-        self-motivated to deliver reliable results.
+        A responsive weather app built with vanilla JavaScript that displays
+        current conditions, hourly and daily forecasts using the OpenWeather
+        API. Features city search, unit toggle (°C/°F), optional browser
+        geolocation, and graceful error handling. Optimized for performance and
+        accessibility with client‑side caching and a clean mobile‑first UI.
       </p>
+      <GitHubLink />
     </section>
   );
 };
