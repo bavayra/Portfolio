33c25d43523087cfbd13224faeb63f9e1e030b42
Alix Bell <x.bavayra@gmail.com>
2026-02-23 16:54:34 +0100

    feat(Footer): added contact fake tel number and adress

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,6 +1,15 @@
 const Footer = () => {
   return (
-    <footer className="py-3 text-center text-xs text-white bg-grey-medium ">
+    <footer className="py-3 text-center text-[0.5rem] text-white bg-grey-medium ">
+      <div className="grid grid-cols-[1fr_max-content_1fr] max-w-52 mx-auto text-[0.65rem]  text-grey-extra-dark font-semibold">
+        <a href="tel:+15550100000" aria-label="Call example number">
+          +1 (555) 010‑0000
+        </a>
+        <span className="text-accent-red font-bold">|</span>
+        <p className="location" aria-label="Location: Prague, Czechia">
+          Prague, Czechia
+        </p>
+      </div>
       <p className="tracking-wider leading-relaxed">
         All Rights Reserved © {new Date().getFullYear()}
       </p>
