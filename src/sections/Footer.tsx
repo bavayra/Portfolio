3d1182489037d5ade95eb1ac1dba03a75e2f91d4
Alix Bell <x.bavayra@gmail.com>
2026-02-24 15:37:27 +0100

    style(Footer): added  a border-t for the section. Increased the small red divider size, set pl and -mt  for it. Deleted mb for the phone+adress element, changed its color to be dynamic for the dark theme

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,11 +1,11 @@
 const Footer = () => {
   return (
-    <footer className="py-3 text-center text-[0.75rem] text-white bg-[#4f4f4f]">
-      <div className="grid grid-cols-[1fr_max-content_1fr] max-w-52 mx-auto text-[0.75rem] mb-2 text-grey-extra-dark font-semibold">
+    <footer className="py-3 text-center text-[0.75rem] border-t-2 border-accent-red text-white bg-[#4f4f4f]">
+      <div className="grid grid-cols-[1fr_max-content_1fr] max-w-52 mx-auto text-[0.75rem]  text-grey-light font-semibold">
         <a href="tel:+15550100000" aria-label="Call example number">
           +1 (555) 010‑0000
         </a>
-        <span className="text-accent-red font-bold">|</span>
+        <span className="text-accent-red font-bold text-lg -mt-1 pl-1">|</span>
         <p className="location" aria-label="Location: Prague, Czechia">
           Prague, Czechia
         </p>
