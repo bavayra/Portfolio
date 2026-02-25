a7c38dc97dcf8bbf5b1a209c977baced7d2879c0
Alix Bell <x.bavayra@gmail.com>
2026-02-25 14:49:49 +0100

    feat: added TW breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      /*default-320px*/
+      "phone-sm": "375px",
+      sm: "640px",
+      md: "768px",
+      tablet: "820px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
   plugins: [],
 };
