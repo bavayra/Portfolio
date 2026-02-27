2d35fd16c8a5a885d1bff4d4b5e5c25f975b71c9
Alix Bell <x.bavayra@gmail.com>
2026-02-27 19:35:16 +0100

    style/refactor(Hero): 360: decreased ml for the divider. 375-480: increased ml for the divider.

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -9,20 +9,20 @@ const Hero = () => {
     <section id="hero" className="py-8 app-fullscreen">
       <Divider
         widthPx={2}
-        heightPx="var(--hero-divider-h)"
+        heightPx={228}
         leftPx={0}
         color="grey-2"
-        topPx="var(--hero-divider-top)"
-        className="ml-6"
+        topPx={"var(--hero-divider-top)"}
+        className="ml-8 min-[390px]:ml-10 min-[428px]:ml-12 min-[480px]:ml-14"
       />
-      <div className="grid grid-cols-1fr grid-rows-1fr">
-        <p className="text-base phone-sm:text-xl text-grey-2 phone-md:text-2xl col-start-1 row-start-1 pl-1">
+      <div className="grid grid-cols-1fr phone-md:ml-4 min-[480px]:ml-0 grid-rows-1fr">
+        <p className="text-base phone-sm:text-xl text-grey-2 min-[390px]:text-2xl col-start-1 row-start-1 pl-1">
           2026
         </p>
-        <h1 className="text-5xl phone-md:text-6xl font-semibold text-grey-2 col-start-1 col-span-3 row-start-2 pl-1">
+        <h1 className="text-5xl phone-sm:text-6xl font-semibold text-grey-2 col-start-1 col-span-3 row-start-2 pl-1">
           Portfolio
         </h1>
-        <p className=" text-accent-red phone-sm:text-xl phone-md:text-3xl font-bold tracking-wider text-base col-start-3 row-start-3 pr-12">
+        <p className=" text-accent-red phone-sm:text-2xl phone-md:text-3xl font-bold tracking-wider text-base col-start-3 row-start-3 pr-12">
           ALIX BELL
         </p>
       </div>
