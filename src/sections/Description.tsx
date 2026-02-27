5cf591f472839e7dcd1fabb608a065aa156f0596
Alix Bell <x.bavayra@gmail.com>
2026-02-27 18:33:50 +0100

    style(Description): for 320 decreased img size. For 412 increased image size and set max-h for the section

--- src/sections/Description.tsx
+++ src/sections/Description.tsx
@@ -2,10 +2,10 @@ import { Divider } from "../components/Divider";
 
 export const Description = () => {
   return (
-    <section className="relative h-130 py-8 ml-3">
+    <section className="relative h-screen phone-md:max-h-[74vh] py-8 ml-3">
       <Divider
         widthPx={2}
-        heightPx={396}
+        heightPx="var(--block-divider-h)"
         leftPx={0}
         color="grey-2"
         topPx={100}
@@ -14,7 +14,7 @@ export const Description = () => {
       <h2 className="text-2xl text-grey-2 font-medium phone-sm:text-3xl">
         WHAT IS SHE LIKE
       </h2>
-      <p className="mt-2 min-[480px]:max-w-80 text-xs phone-sm:text-sm phone-md:text-base phone-md:max-w-76 phone-sm:max-w-68 text-text leading-loose tracking-wide ml-3 w-full max-w-60 wrap-break-words whitespace-normal">
+      <p className="mt-2 phone-md:max-w-80 text-xs phone-sm:text-sm phone-md:text-base phone-sm:max-w-68 text-text leading-loose tracking-wide ml-3 w-full max-w-60 wrap-break-words whitespace-normal">
         I'm a curious and disciplined developer who enjoys solving UI problems
         with clean, maintainable code. I prioritize performance and
         accessibility, and I'm committed to continuous learning. I work well in
@@ -25,11 +25,11 @@ export const Description = () => {
       <img
         src="/fonts/images/AuthorImgAI.webp"
         alt="AI-generated portrait of the author"
-        width={140}
+        width={128}
         height={200}
         style={{ position: "absolute", right: 0, bottom: 0 }}
         loading="lazy"
-        className="object-cover h-auto "
+        className="object-cover phone-md:w-40 "
       />
     </section>
   );
