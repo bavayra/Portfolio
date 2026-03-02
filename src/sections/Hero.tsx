921398a67adf41699c4a8722a29a8b26e37e8aea
Alix Bell <x.bavayra@gmail.com>
2026-03-02 16:49:38 +0100

    style(Hero-480): added mt for the text block and increased font size of h1 and year elements

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -13,13 +13,13 @@ const Hero = () => {
         leftPx={0}
         color="grey-2"
         topPx={"var(--hero-divider-top)"}
-        className="ml-8 min-[390px]:ml-10 min-[428px]:ml-12 min-[480px]:ml-14"
+        className="ml-8 min-[390px]:ml-10 min-[428px]:ml-12 phone-lg:ml-10"
       />
-      <div className="grid grid-cols-1fr phone-md:ml-4 min-[480px]:ml-0 grid-rows-1fr">
-        <p className="text-base phone-sm:text-xl text-grey-2 min-[390px]:text-2xl col-start-1 row-start-1 pl-3 ">
+      <div className="grid grid-cols-1fr phone-md:ml-4 phone-lg:mt-10 grid-rows-1fr">
+        <p className="text-base phone-sm:text-xl text-grey-2 min-[390px]:text-2xl phone-lg:text-3xl col-start-1 row-start-1 pl-3 ">
           2026
         </p>
-        <h1 className="text-5xl phone-sm:text-6xl font-semibold text-grey-2 col-start-1 col-span-3 row-start-2 pl-3 min-[375px]:pl-1">
+        <h1 className="text-5xl phone-sm:text-6xl font-semibold text-grey-2 col-start-1 col-span-3 row-start-2 pl-3 phone-lg:text-7xl min-[375px]:pl-1">
           Portfolio
         </h1>
         <p className=" text-accent-red phone-sm:text-2xl phone-md:text-3xl font-bold tracking-wider text-base col-start-3 min-[375px]:pr-10 row-start-3 pr-8">
