3d63663a65ca869ed60b684064d385bff69121a0
Alix Bell <x.bavayra@gmail.com>
2026-03-02 16:18:53 +0100

    style(Hero-375): decreased pl for h1 and increased pr for the name

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -16,13 +16,13 @@ const Hero = () => {
         className="ml-8 min-[390px]:ml-10 min-[428px]:ml-12 min-[480px]:ml-14"
       />
       <div className="grid grid-cols-1fr phone-md:ml-4 min-[480px]:ml-0 grid-rows-1fr">
-        <p className="text-base phone-sm:text-xl text-grey-2 min-[390px]:text-2xl col-start-1 row-start-1 pl-3">
+        <p className="text-base phone-sm:text-xl text-grey-2 min-[390px]:text-2xl col-start-1 row-start-1 pl-3 ">
           2026
         </p>
-        <h1 className="text-5xl phone-sm:text-6xl font-semibold text-grey-2 col-start-1 col-span-3 row-start-2 pl-3">
+        <h1 className="text-5xl phone-sm:text-6xl font-semibold text-grey-2 col-start-1 col-span-3 row-start-2 pl-3 min-[375px]:pl-1">
           Portfolio
         </h1>
-        <p className=" text-accent-red phone-sm:text-2xl phone-md:text-3xl font-bold tracking-wider text-base col-start-3 row-start-3 pr-8">
+        <p className=" text-accent-red phone-sm:text-2xl phone-md:text-3xl font-bold tracking-wider text-base col-start-3 min-[375px]:pr-10 row-start-3 pr-8">
           ALIX BELL
         </p>
       </div>
