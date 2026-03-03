a49fead8b841b5899d0362f817bbca4593b50791
Alix Bell <x.bavayra@gmail.com>
2026-03-03 16:55:01 +0100

    style(description-768): increased img's font size and decreased h of the section

--- src/sections/Description.tsx
+++ src/sections/Description.tsx
@@ -2,7 +2,7 @@ import { Divider } from "../components/Divider";
 
 export const Description = () => {
   return (
-    <section className="relative h-[84vh] tablet-sm:ml-12 min-[375px]:h-[92vh] min-[390px]:h-[70vh] phone-md:max-h-[74vh] min-[428px]:h-[68vh] phone-lg:h-[72vh] py-8 ml-3">
+    <section className="relative h-[84vh] tablet-sm:ml-12 min-[375px]:h-[92vh] tablet-md:h-[60vh] min-[390px]:h-[70vh] phone-md:max-h-[74vh] min-[428px]:h-[68vh] phone-lg:h-[72vh] py-8 ml-3">
       <Divider
         widthPx={2}
         heightPx="var(--block-div-xl-h)"
@@ -29,7 +29,7 @@ export const Description = () => {
         height={200}
         style={{ position: "absolute", right: 0, bottom: 0 }}
         loading="lazy"
-        className="object-cover phone-md:w-36 phone-lg:w-40 tablet-sm:w-48 "
+        className="object-cover phone-md:w-36 phone-lg:w-40 tablet-sm:w-48 tablet-md:w-60"
       />
     </section>
   );
