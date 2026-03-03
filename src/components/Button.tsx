d25549c14e1deda2bcd26c8d80f1e40e682f8a5b
Alix Bell <x.bavayra@gmail.com>
2026-03-03 16:53:17 +0100

    style(About-768): increased font size for btn secondary small variant, increased its mr and py

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -22,7 +22,7 @@ const Button = ({
   ...props
 }: ButtonProps) => {
   const baseStyles =
-    "rounded-sm transform transition-all duration-300 drop-shadow-md px-6 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed";
+    "rounded-sm transform transition-all duration-300 drop-shadow-md tablet-md:py-3 px-6 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed";
   const variantStyles: Record<ButtonVariant, string> = {
     primary:
       "bg-section-title text-white hover:bg-grey-3 hover:scale-105 focus:ring-accent-red active:bg-grey-3",
@@ -31,7 +31,7 @@ const Button = ({
   };
   const sizeStyles: Record<ButtonSize, string> = {
     small:
-      "px-3 py-2 text-xs phone-sm:text-sm phone-lg:text-base tablet-sm:text-lg",
+      "px-3 py-2 text-xs phone-sm:text-sm phone-lg:text-base tablet-sm:text-lg tablet-md:text-xl",
     medium: "px-4 py-2 text-base ",
     large: "px-4 py-1 text-lg",
   };
