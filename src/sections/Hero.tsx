746a3c069e13672fd523a278ce287ec0c3fdfe7b
Alix Bell <x.bavayra@gmail.com>
2026-03-03 18:25:12 +0100

    refactor(font-size): added a responsive clamp global variable for year's text  font size and reused it with length prefix

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
         className="ml-8 min-[390px]:ml-10 min-[428px]:ml-12 tablet-sm:ml-12 phone-lg:ml-10"
       />
       <div className="grid grid-cols-1fr phone-md:ml-4 tablet-md:mt-40 phone-lg:mt-10 tablet-sm:mt-26 tablet-sm:ml-8 grid-rows-1fr">
-        <p className="text-base phone-sm:text-xl text-grey-2 min-[390px]:text-2xl phone-lg:text-3xl col-start-1 row-start-1 pl-3 ">
+        <p className=" text-[length:var(--year-size)] text-grey-2 col-start-1 row-start-1 pl-3 ">
           2026
         </p>
         <h1 className=" text-[length:var(--h1-size)] font-semibold text-grey-2 col-start-1  tablet-sm:text-8xl col-span-3 row-start-2 pl-3 min-[375px]:pl-1">
