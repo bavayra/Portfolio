0fcfef200f564c7589eeea45fab467614ede1817
Alix Bell <x.bavayra@gmail.com>
2026-03-03 16:31:59 +0100

    style(Hero-768): increased mt for the text elements, increased negative bottom position value, increased pr for the name element. Increased divider's h

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -6,10 +6,7 @@ import { Divider } from "../components/Divider";
 
 const Hero = () => {
   return (
-    <section
-      id="hero"
-      className="py-8 app-fullscreen relative  overflow-hidden"
-    >
+    <section id="hero" className="py-8 app-fullscreen relative overflow-hidden">
       <Divider
         widthPx="var(--hero-div-w)"
         heightPx="var(--hero-div-h)"
@@ -18,14 +15,14 @@ const Hero = () => {
         topPx="var(--hero-div-top)"
         className="ml-8 min-[390px]:ml-10 min-[428px]:ml-12 tablet-sm:ml-12 phone-lg:ml-10"
       />
-      <div className="grid grid-cols-1fr phone-md:ml-4 phone-lg:mt-10 tablet-sm:mt-26 tablet-sm:ml-8 grid-rows-1fr">
+      <div className="grid grid-cols-1fr phone-md:ml-4 tablet-md:mt-40 phone-lg:mt-10 tablet-sm:mt-26 tablet-sm:ml-8 grid-rows-1fr">
         <p className="text-base phone-sm:text-xl text-grey-2 min-[390px]:text-2xl phone-lg:text-3xl col-start-1 row-start-1 pl-3 ">
           2026
         </p>
         <h1 className="text-5xl phone-sm:text-6xl font-semibold text-grey-2 col-start-1  tablet-sm:text-8xl col-span-3 row-start-2 pl-3 phone-lg:text-7xl min-[375px]:pl-1">
           Portfolio
         </h1>
-        <p className=" text-accent-red phone-sm:text-2xl phone-md:text-3xl tablet-sm:text-4xl font-bold tracking-wider text-base col-start-3 min-[375px]:pr-10 row-start-3 pr-8">
+        <p className=" text-accent-red  phone-sm:text-2xl tablet-md:pr-30 phone-md:text-3xl tablet-sm:text-4xl font-bold tracking-wider text-base col-start-3 min-[375px]:pr-10 row-start-3 pr-8">
           ALIX BELL
         </p>
       </div>
