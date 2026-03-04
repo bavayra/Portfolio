d8dd552fe021ae47d1224720079d79efab77ed37
Alix Bell <x.bavayra@gmail.com>
2026-03-04 18:46:18 +0100

    style(Hero-480): deleted ml for the divider, increased leading and year's and name's text elements' sizes

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -9,20 +9,20 @@ const Hero = () => {
     <section id="hero" className="py-8 app-fullscreen relative overflow-hidden">
       <Divider
         widthPx="var(--hero-div-w)"
-        heightPx={112}
+        heightPx={200}
         leftPx={0}
         color="grey-2"
-        topPx="var(--hero-div-top)"
-        className="ml-8 min-[390px]:ml-10 min-[428px]:ml-12 tablet-sm:ml-12 phone-lg:ml-10"
+        topPx={180}
+        className="ml-8 tablet-sm:ml-14"
       />
-      <div className="grid grid-cols-1fr ml-4 phone-md:ml-4 leading-12 tablet-md:mt-40 tablet-lg:mt-52 phone-lg:mt-10 tablet-sm:mt-26 tablet-sm:ml-8 grid-rows-1fr">
-        <p className=" text-2xl text-grey-2 tablet-lg:text-4xl col-start-1 row-start-1 pl-3 ">
+      <div className="grid grid-cols-1fr ml-4 min-[390px]:mt-16 phone-md:ml-4 min-[428px]:leading-16 leading-12 tablet-md:mt-40 tablet-lg:mt-52 phone-lg:mt-10 tablet-sm:mt-26 tablet-sm:ml-8 grid-rows-1fr">
+        <p className=" text-3xl text-grey-2 tablet-lg:text-4xl col-start-1 row-start-1 pl-3 ">
           2026
         </p>
         <h1 className=" text-[length:var(--h1-size)] font-semibold text-grey-2 tablet-lg:text-9xl col-start-1 tablet-sm:text-8xl col-span-3 row-start-2 pl-3 ">
           Portfolio
         </h1>
-        <p className="text-2xl tablet-lg:text-5xl tablet-lg:pr-20  text-accent-red tablet-md:pr-30 tablet-sm:text-4xl font-bold tracking-wider col-start-3 min-[375px]:pr-10 row-start-3 pr-8">
+        <p className="text-3xl tablet-lg:text-5xl tablet-lg:pr-20  text-accent-red tablet-md:pr-30 tablet-sm:text-4xl font-bold tracking-wider col-start-3 min-[375px]:pr-10 row-start-3 pr-8">
           ALIX BELL
         </p>
       </div>
