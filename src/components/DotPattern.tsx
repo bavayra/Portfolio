97488754bd80d3b8dec8e69533bf7e8a8d7931ac
Alix Bell <x.bavayra@gmail.com>
2026-03-04 17:49:03 +0100

    style(DotPattern-screens>600): increased pl for the pattern, added translate-y for 600 and 768 pattern sizing

--- src/components/DotPattern.tsx
+++ src/components/DotPattern.tsx
@@ -1,11 +1,11 @@
 const DotPattern = () => {
   return (
-    <div className="absolute bottom-0 inset-x-0 w-screen left-1/2 -translate-x-1/2 tablet-sm:-bottom-20 tablet-md:-bottom-72">
+    <div className="absolute bottom-0 inset-x-0 w-screen left-1/2 -translate-x-1/2 tablet-sm:translate-y-[12%] tablet-md:translate-y-[24%]">
       <svg
         width="100%"
         height="100%"
         viewBox="0 0 180 180"
-        className="tablet-sm:scale-140 w-full h-full pl-5 block tablet-sm:origin-bottom-center "
+        className="tablet-sm:pl-8  w-full h-full pl-5 block"
       >
         <defs>
           <pattern
