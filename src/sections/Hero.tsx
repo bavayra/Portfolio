c51958fc7116d2643a6db7e00453493a6359a118
Alix Bell <x.bavayra@gmail.com>
2026-03-04 19:08:01 +0100

    style(Hero-390): decreased mt for text elements' block

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -15,11 +15,11 @@ const Hero = () => {
         topPx={24}
         className="ml-8 tablet-sm:ml-14"
       />
-      <div className="grid grid-cols-1fr ml-4 min-[390px]:mt-16 phone-md:ml-4 min-[428px]:leading-16 leading-12 tablet-md:mt-40 tablet-lg:mt-52 phone-lg:mt-10 tablet-sm:mt-36 tablet-sm:ml-8 grid-rows-1fr">
+      <div className="grid grid-cols-1fr ml-4 min-[390px]:mt-10 phone-md:ml-4 min-[428px]:leading-16 leading-12 tablet-md:mt-40 tablet-lg:mt-52 phone-lg:mt-10 tablet-sm:mt-36 tablet-sm:ml-8 grid-rows-1fr">
         <p className=" text-3xl text-grey-2 tablet-lg:text-4xl col-start-1 row-start-1 pl-3 ">
           2026
         </p>
-        <h1 className=" text-[length:var(--h1-size)] font-semibold text-grey-2 tablet-lg:text-9xl col-start-1 tablet-sm:text-8xl col-span-3 row-start-2 pl-3 ">
+        <h1 className=" text-7xl font-semibold text-grey-2 tablet-lg:text-9xl col-start-1 tablet-sm:text-8xl col-span-3 row-start-2 pl-3 ">
           Portfolio
         </h1>
         <p className="text-3xl tablet-lg:text-5xl tablet-lg:pr-20  text-accent-red tablet-md:pr-30 tablet-sm:text-4xl font-bold tracking-wider col-start-3 min-[375px]:pr-10 row-start-3 pr-8">
