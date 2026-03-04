6ba84b27c6b1427edecc777076f4f34a1c8aae68
Alix Bell <x.bavayra@gmail.com>
2026-03-04 13:59:47 +0100

    style(Hero-820): increased font sizes for all text elements and increased mt for its block. Decreased pr for the name element

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -15,14 +15,14 @@ const Hero = () => {
         topPx="var(--hero-div-top)"
         className="ml-8 min-[390px]:ml-10 min-[428px]:ml-12 tablet-sm:ml-12 phone-lg:ml-10"
       />
-      <div className="grid grid-cols-1fr phone-md:ml-4 tablet-md:mt-40 phone-lg:mt-10 tablet-sm:mt-26 tablet-sm:ml-8 grid-rows-1fr">
-        <p className=" text-[length:var(--year-size)] text-grey-2 col-start-1 row-start-1 pl-3 ">
+      <div className="grid grid-cols-1fr phone-md:ml-4 tablet-md:mt-40 tablet-lg:mt-52 phone-lg:mt-10 tablet-sm:mt-26 tablet-sm:ml-8 grid-rows-1fr">
+        <p className=" text-[length:var(--year-size)] text-grey-2 tablet-lg:text-4xl col-start-1 row-start-1 pl-3 ">
           2026
         </p>
-        <h1 className=" text-[length:var(--h1-size)] font-semibold text-grey-2 col-start-1  tablet-sm:text-8xl col-span-3 row-start-2 pl-3 min-[375px]:pl-1">
+        <h1 className=" text-[length:var(--h1-size)] font-semibold text-grey-2 tablet-lg:text-9xl col-start-1 tablet-sm:text-8xl col-span-3 row-start-2 pl-3 min-[375px]:pl-1">
           Portfolio
         </h1>
-        <p className="text-[length:var(--name-size)]  text-accent-red tablet-md:pr-30 tablet-sm:text-4xl font-bold tracking-wider col-start-3 min-[375px]:pr-10 row-start-3 pr-8">
+        <p className="text-[length:var(--name-size)] tablet-lg:text-5xl tablet-lg:pr-20  text-accent-red tablet-md:pr-30 tablet-sm:text-4xl font-bold tracking-wider col-start-3 min-[375px]:pr-10 row-start-3 pr-8">
           ALIX BELL
         </p>
       </div>
