d2ad56b0a26ccb1e8dd65fb0cadc67638f98373c
Alix Bell <x.bavayra@gmail.com>
2026-03-07 17:12:35 +0100

    refactor(Dividers-description): recalculated clamps for block-div-xl height for screens 360-820

--- src/sections/Description.tsx
+++ src/sections/Description.tsx
@@ -5,7 +5,7 @@ export const Description = () => {
     <section className="relative h-[84vh] tablet-sm:ml-12 min-[375px]:h-[92vh] tablet-md:h-[60vh] min-[390px]:h-[70vh] phone-md:max-h-[74vh] min-[428px]:h-[68vh] phone-lg:h-[72vh] py-8 ml-3">
       <Divider
         widthPx="var(--hero-div-w)"
-        heightPx="var(--block-div-xl-h)"
+        heightPx="var(--block-div-lg-h)"
         leftPx={0}
         color="grey-2"
         topPx="var(--block-div-top)"
