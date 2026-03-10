fc0a039cea866c4f5aa1bf2895bd553ed536f989
Alix Bell <x.bavayra@gmail.com>
2026-03-10 20:48:36 +0100

    fix(dot-pattern): add valid desktop pattern definition and switch on 1024+

--- src/components/DotPattern.tsx
+++ src/components/DotPattern.tsx
@@ -1,23 +1,42 @@
 const DotPattern = () => {
   return (
-    <div className="absolute bottom-0 inset-x-0 w-screen left-1/2 -translate-x-1/2 tablet-sm:translate-y-[12%] tablet-md:translate-y-[24%]">
+    <div className="absolute bottom-0 desktop-sm:translate-x-0.5 inset-x-0 w-screen left-1/2 -translate-x-1/2 tablet-sm:translate-y-[12%] tablet-md:translate-y-[24%]">
       <svg
         width="100%"
         height="100%"
         viewBox="0 0 180 180"
-        className="tablet-sm:pl-8  w-full h-full pl-5 block"
+        className="tablet-sm:pl-8 w-full h-full pl-5 block"
       >
         <defs>
           <pattern
             id="dot-pattern"
-            width="20"
-            height="20"
+            patternUnits="userSpaceOnUse"
+            width={20}
+            height={20}
+          >
+            <circle
+              cx="3"
+              cy="3"
+              fill="var(--color-accent-red)"
+              id="dot"
+              r={3}
+            />
+          </pattern>
+          <pattern
+            id="dot-pattern-lg"
+            width={12}
+            height={12}
             patternUnits="userSpaceOnUse"
           >
-            <circle cx="3" cy="3" r="3" fill="var(--color-accent-red)" />
+            <circle cx="3" cy="3" r={2} fill="var(--color-accent-red)" />
           </pattern>
         </defs>
-        <rect width="100%" height="100%" fill="url(#dot-pattern)" />
+        <rect
+          id="dot-pattern-rect"
+          width="100%"
+          height="100%"
+          fill="url(#dot-pattern)"
+        />
       </svg>
     </div>
   );
