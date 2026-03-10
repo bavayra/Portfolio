89fb66a8e79cb3246f88333e51fe2d8d5983f6b8
Alix Bell <x.bavayra@gmail.com>
2026-03-10 21:37:01 +0100

    style(Hero-1024): positioned decor pattern and text elements correctly. Corrected text elements' font sizes. Moved the name element to the 2 row.

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -9,24 +9,31 @@ const Hero = () => {
     <section id="hero" className="py-8 app-fullscreen relative overflow-hidden">
       <Divider
         widthPx="var(--hero-div-w)"
-        heightPx={148}
+        heightPx="var(--hero-div-height)"
         leftPx={0}
         color="grey-2"
-        topPx={24}
+        topPx="var(--hero-div-top)"
         className="ml-8 tablet-sm:ml-14"
       />
-      <div className="grid grid-cols-1fr ml-4 min-[390px]:mt-10 phone-md:ml-4 min-[428px]:leading-16 leading-12 tablet-md:mt-40 tablet-lg:mt-52 phone-lg:mt-10 tablet-sm:mt-36 tablet-sm:ml-8 grid-rows-1fr">
-        <p className=" text-3xl text-grey-2 tablet-lg:text-4xl col-start-1 row-start-1 pl-3 ">
+      <div className="grid grid-cols-1fr ml-4 desktop-sm:translate-y-full desktop-sm:mt-0 min-[390px]:mt-10 phone-md:ml-4 phone-lg:leading-14 tablet-sm:leading-16 leading-10 tablet-md:mt-40 tablet-lg:mt-52 phone-lg:mt-10 tablet-sm:mt-36 tablet-sm:ml-8 grid-rows-1fr">
+        <p className=" text-[length:var(--name-year-size)] text-grey-2 tablet-lg:text-4xl col-start-1 row-start-1 pl-3 ">
           2026
         </p>
-        <h1 className="text-[length:var(--h1-size)] font-semibold text-grey-2 tablet-lg:text-9xl col-start-1 col-span-3 row-start-2 pl-3 ">
+        <h1 className="text-[length:var(--h1-size)] desktop-sm:text-8xl font-semibold text-grey-2 tablet-lg:text-9xl col-start-1 col-span-3 row-start-2 pl-3 ">
           Portfolio
         </h1>
-        <p className="text-[length:var(--name-size)] tablet-lg:text-5xl tablet-lg:pr-20  text-accent-red tablet-md:pr-30 font-bold tracking-wider col-start-3 min-[375px]:pr-10 row-start-3 pr-8">
+        <p className="text-[length:var(--name-year-size)] desktop-sm:col-start-2 desktop-sm:text-4xl tablet-lg:text-5xl tablet-lg:pr-20  text-accent-red tablet-md:pr-30 font-bold tracking-wider col-start-3 min-[375px]:pr-10 row-start-3 pr-8">
           ALIX BELL
         </p>
       </div>
-
+      <Divider
+        widthPx="var(--hero-div-w)"
+        heightPx="var(--hero-div-height)"
+        leftPx={0}
+        color="grey-2"
+        topPx="var(--hero-div-top)"
+        className="ml-8 tablet-sm:ml-14 hidden desktop-sm:block"
+      />
       <DotPattern />
     </section>
   );
