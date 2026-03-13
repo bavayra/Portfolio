27bd166d0bc7173dcafbbf844458cfcfd54f520c
Alix Bell <x.bavayra@gmail.com>
2026-03-13 19:17:25 +0100

    style(Skills-1728): increased labels' font size. Increased badges' scale. Increased gaps between links

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -29,7 +29,7 @@ const Skills = () => {
       <h2 className="text-2xl desktop-sm:flex desktop-sm:justify-end desktop-sm:mr-10 tablet-md:my-2 desktop-sm:text-4xl phone-lg:text-4xl text-grey-2 tablet-md:text-5xl phone-sm:text-3xl font-medium">
         HER SKILLS
       </h2>
-      <div className="grid grid-cols-3 desktop-sm:gap-x-0 desktop-sm:gap-y-6 gap-3 mt-4 tablet-sm:mt-10 phone-sm:mt-8 justify-items-center phone-md:gap-x-12 tablet-sm:gap-y-12 gap-x-0 w-3xs phone-sm:gap-y-6 tablet-sm:w-sm mx-auto">
+      <div className="grid grid-cols-3 desktop-sm:gap-x-0 desktop-sm:gap-y-6 gap-3 mt-4 desktop-xl:gap-y-10 desktop-xl:gap-x-16 tablet-sm:mt-10 phone-sm:mt-8 justify-items-center phone-md:gap-x-12 tablet-sm:gap-y-12 gap-x-0 w-3xs phone-sm:gap-y-6 tablet-sm:w-sm mx-auto">
         {skills.map(({ percent, label }) => (
           <div
             key={label}
@@ -38,9 +38,9 @@ const Skills = () => {
             <CircleStatic
               percent={percent}
               ariaLabel={label}
-              className="phone-sm:scale-120 desktop-sm:scale-130 tablet-sm:scale-150 phone-sm:[--skill-percent-size:.8rem]"
+              className="phone-sm:scale-120 desktop-sm:scale-130 desktop-xl:scale-160 tablet-sm:scale-150 phone-sm:[--skill-percent-size:.8rem]"
             />
-            <span className="text-[0.65rem] desktop-sm:text-sm text-text phone-sm:text-xs tablet-sm:text-base phone-lg:text-sm">
+            <span className="text-[0.65rem] desktop-sm:text-sm desktop-xl:text-base text-text phone-sm:text-xs tablet-sm:text-base phone-lg:text-sm">
               {label}
             </span>
           </div>
