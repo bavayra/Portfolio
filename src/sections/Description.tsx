79a392a4febcb8c5387d3abd5471b9312ce9fab0
Alix Bell <x.bavayra@gmail.com>
2026-03-13 15:47:59 +0100

    style(Description-1024): increased section's h. Decreased text block's font size and its w. Decreased h2 font size. Increased img's size and added mr for it. Decreased height of the lg block divider variant

--- src/sections/Description.tsx
+++ src/sections/Description.tsx
@@ -2,7 +2,7 @@ import { Divider } from "../components/Divider";
 
 export const Description = () => {
   return (
-    <section className="relative h-[84vh] tablet-sm:ml-12 min-[375px]:h-[92vh] tablet-md:h-[60vh] min-[390px]:h-[70vh] phone-md:max-h-[74vh] min-[428px]:h-[68vh] phone-lg:h-[72vh] py-8 ml-3">
+    <section className="relative h-[84vh] desktop-sm:h-[70vh] tablet-sm:ml-12 min-[375px]:h-[92vh] tablet-md:h-[60vh] min-[390px]:h-[70vh] phone-md:max-h-[74vh] min-[428px]:h-[68vh] phone-lg:h-[72vh] py-8 ml-3">
       <Divider
         widthPx="var(--hero-div-w)"
         heightPx="var(--block-div-lg-h)"
@@ -11,10 +11,10 @@ export const Description = () => {
         topPx="var(--block-div-top)"
       />
       <p className="text-text phone-sm:text-xl tablet-sm:text-2xl">03</p>
-      <h2 className="text-2xl text-grey-2 tablet-md:my-2 phone-lg:text-4xl tablet-md:text-5xl font-medium phone-sm:text-3xl">
+      <h2 className="text-2xl text-grey-2 desktop-sm:text-4xl tablet-md:my-2 phone-lg:text-4xl tablet-md:text-5xl font-medium phone-sm:text-3xl">
         WHAT IS SHE LIKE
       </h2>
-      <p className="mt-2 tablet-sm:text-lg tablet-md:max-w-116 tablet-sm:max-w-92 phone-md:max-w-80 text-xs phone-sm:text-sm phone-lg:pl-2 phone-md:text-base phone-sm:max-w-68 text-text leading-loose tracking-wide ml-3 w-full max-w-60 wrap-break-words whitespace-normal">
+      <p className="mt-2 desktop-sm:max-w-92 desktop-sm:text-base tablet-sm:text-lg tablet-md:max-w-116 tablet-sm:max-w-92 phone-md:max-w-80 text-xs phone-sm:text-sm phone-lg:pl-2 phone-md:text-base phone-sm:max-w-68 text-text leading-loose tracking-wide ml-3 w-full max-w-60 wrap-break-words whitespace-normal">
         I'm a curious and disciplined developer who enjoys solving UI problems
         with clean, maintainable code. I prioritize performance and
         accessibility, and I'm committed to continuous learning. I work well in
@@ -29,7 +29,7 @@ export const Description = () => {
         height={200}
         style={{ position: "absolute", right: 0, bottom: 0 }}
         loading="lazy"
-        className="object-cover phone-md:w-36 phone-lg:w-40 tablet-sm:w-48 tablet-md:w-60"
+        className="object-cover phone-md:w-36 desktop-sm:w-72 desktop-sm:mr-24 phone-lg:w-40 tablet-sm:w-48 tablet-md:w-60"
       />
     </section>
   );
