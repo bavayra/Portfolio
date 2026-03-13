b668121d47a391f85e6e7294f9417c7bc30f4e3e
Alix Bell <x.bavayra@gmail.com>
2026-03-13 15:51:57 +0100

    style(Projects - 1024): for all projects' sections (DogShelter, Portfolio> WeatherApp) decreased h2's font size. Decreased text block's w and its font size

--- src/sections/DogShelter.tsx
+++ src/sections/DogShelter.tsx
@@ -12,10 +12,10 @@ export const DogShelter = () => {
         topPx="var(--block-div-top)"
       />
       <p className="text-text phone-sm:text-xl tablet-sm:text-2xl">06</p>
-      <h2 className="text-2xl text-grey-2 tablet-md:my-2  phone-sm:text-3xl phone-lg:text-4xl tablet-md:text-5xl font-medium">
+      <h2 className="text-2xl desktop-sm:text-4xl text-grey-2 tablet-md:my-2  phone-sm:text-3xl phone-lg:text-4xl tablet-md:text-5xl font-medium">
         DOG SHELTER WEB
       </h2>
-      <p className="mt-2 phone-lg:max-w-80 tablet-md:max-w-116 text-xs phone-lg:pl-2 tablet-sm:text-lg tablet-sm:max-w-92 phone-sm:text-sm phone-md:text-base phone-md:max-w-76 phone-sm:max-w-68 text-text leading-loose tracking-wide ml-3 w-full max-w-60 wrap-break-words whitespace-normal">
+      <p className="mt-2 phone-lg:max-w-80 desktop-sm:max-w-92 desktop-sm:text-base tablet-md:max-w-116 text-xs phone-lg:pl-2 tablet-sm:text-lg tablet-sm:max-w-92 phone-sm:text-sm phone-md:text-base phone-md:max-w-76 phone-sm:max-w-68 text-text leading-loose tracking-wide ml-3 w-full max-w-60 wrap-break-words whitespace-normal">
         A responsive web app for a dog shelter built with React and TypeScript.
         It features a searchable pet gallery with different filters, adoption
         rules, contact/donation section, and accessible modals for each dog. The
