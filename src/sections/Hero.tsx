975f63605b63ade31581085447dbd18eddaa3b90
Alix Bell <x.bavayra@gmail.com>
2026-03-17 20:38:16 +0100

    perf/refactor: cleaning up the hero section using fluid typo

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -18,14 +18,14 @@ const Hero = () => {
         topPx="var(--hero-div-top)"
         className="ml-8 tablet-sm:ml-16 desktop-lg:ml-14"
       />
-      <div className="grid grid-cols-1fr ml-4 desktop-sm:translate-y-full desktop-sm:mt-0 min-[390px]:mt-10 phone-md:ml-4 phone-lg:leading-14 tablet-sm:leading-16 leading-10 tablet-md:mt-40 tablet-lg:mt-52 phone-lg:mt-10 tablet-sm:mt-36 tablet-sm:ml-8 grid-rows-1fr">
-        <p className=" text-[length:var(--name-year-size)] text-grey-2 tablet-lg:text-4xl col-start-1 row-start-1 pl-3 ">
+      <div className="grid grid-cols-1fr mt-[var(--hero-mt)] ml-4 desktop-sm:translate-y-[75%] desktop-sm:leading-18 desktop-lg:leading-20 phone-md:ml-4 phone-lg:leading-12 tablet-sm:leading-16 leading-10 tablet-sm:ml-8 grid-rows-1fr">
+        <p className=" text-[length:var(--name-year-size)] text-grey-2 col-start-1 row-start-1 pl-3 ">
           2026
         </p>
-        <h1 className="text-[length:var(--h1-size)] desktop-lg:text-9xl desktop-sm:text-8xl font-semibold text-grey-2 tablet-lg:text-9xl col-start-1 col-span-3 row-start-2 pl-3 ">
+        <h1 className="text-[length:var(--h1-size)] font-semibold text-grey-2 col-start-1 col-span-3 row-start-2 pl-3 ">
           Portfolio
         </h1>
-        <p className="text-[length:var(--name-year-size)] desktop-lg:text-5xl desktop-lg:mr-0 desktop-md:mr-80 desktop-sm:col-start-2 desktop-sm:text-4xl tablet-lg:text-5xl tablet-lg:pr-20  text-accent-red tablet-md:pr-30 font-bold tracking-wider col-start-3 min-[375px]:pr-10 row-start-3 pr-8">
+        <p className="text-[length:var(--name-year-size)] desktop-md:mr-16 desktop-sm:col-start-2 desktop-xl:mr-60 desktop-sm:mr-40 tablet-lg:pr-20  text-accent-red tablet-md:pr-30 font-bold tracking-wider col-start-3 min-[375px]:pr-10 row-start-3 pr-8">
           ALIX BELL
         </p>
       </div>
