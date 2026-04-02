50e9a6a97a5186f8d91f378b86f7ecf2bf923fd8
Alix Bell <x.bavayra@gmail.com>
2026-04-02 18:45:37 +0200

    style(Contacts-360): increased height of the bottom divider, increased font size for h2

--- src/sections/Contacts.tsx
+++ src/sections/Contacts.tsx
@@ -103,7 +103,7 @@ const ContactSection = () => {
         color="grey-2"
         topPx={-68}
       />
-      <h2 className=" relative self-start text-4xl phone-md:text-5xl desktop-sm:text-7xl tablet-md:text-6xl text-accent-red font-bold">
+      <h2 className=" relative self-start text-5xl desktop-sm:text-7xl tablet-md:text-6xl text-accent-red font-bold">
         LET'S TALK?
       </h2>
       <Divider
