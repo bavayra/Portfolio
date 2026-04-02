3c762e7c1397dc44c9fc20e49402342ded9ff5b0
Alix Bell <x.bavayra@gmail.com>
2026-04-02 16:46:09 +0200

    style(Hero-1280): corrected margins. Corrected hero's dividers' height and top positioning

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,8 +1,5 @@
 import DotPattern from "../components/DotPattern";
 import { Divider } from "../components/Divider";
-{
-  /*import { Circle } from "../components/Circle";*/
-}
 
 const Hero = () => {
   return (
@@ -18,14 +15,14 @@ const Hero = () => {
         topPx="var(--hero-div-top)"
         className="ml-8 tablet-sm:ml-16 desktop-lg:ml-14"
       />
-      <div className="grid grid-cols-1fr mt-[var(--hero-mt)] ml-4 desktop-sm:translate-y-[75%] desktop-sm:leading-18 desktop-lg:leading-20 phone-md:ml-4 phone-lg:leading-12 tablet-sm:leading-16 leading-10 tablet-sm:ml-8 grid-rows-1fr">
+      <div className="grid grid-cols-1fr mt-[var(--hero-mt)] ml-4 desktop-sm:translate-y-[75%] desktop-md:ml-10 desktop-sm:leading-18 desktop-lg:leading-20 phone-md:ml-4 phone-lg:leading-12 tablet-sm:leading-16 leading-10 tablet-sm:ml-8 grid-rows-1fr">
         <p className=" text-[length:var(--name-year-size)] text-grey-2 col-start-1 row-start-1 pl-3 ">
           2026
         </p>
         <h1 className="text-[length:var(--h1-size)] font-semibold text-grey-2 col-start-1 col-span-3 row-start-2 pl-3 ">
           Portfolio
         </h1>
-        <p className="text-[length:var(--name-year-size)] desktop-md:mr-16 desktop-sm:col-start-2 desktop-xl:mr-60 desktop-sm:mr-40 tablet-lg:pr-20  text-accent-red tablet-md:pr-30 font-bold tracking-wider col-start-3 min-[375px]:pr-10 row-start-3 pr-8">
+        <p className="text-[length:var(--name-year-size)] desktop-md:mr-16 desktop-sm:col-start-2 desktop-xl:mr-60 desktop-sm:mr-40 tablet-lg:pr-20  text-accent-red tablet-md:pr-30 font-bold desktop-md:mt-4 tracking-wider col-start-3 min-[375px]:pr-10 row-start-3 pr-8">
           ALIX BELL
         </p>
       </div>
