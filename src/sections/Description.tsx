e97bb20f8a5b4e65f0d472cd4bfd22427513e731
Alix Bell <x.bavayra@gmail.com>
2026-04-03 19:17:44 +0200

    style(Description-480): decreased section's h; decreased text continer's w. Increased w of the img and set -mr to move it to the right

--- src/sections/Description.tsx
+++ src/sections/Description.tsx
@@ -2,7 +2,7 @@ import { Divider } from "../components/Divider";
 
 export const Description = () => {
   return (
-    <section className="relative phone-sm:h-[84vh] h-screen desktop-lg:ml-10 desktop-sm:h-[68vh] desktop-lg:h-[60vh] tablet-lg:h-[48vh] desktop-xl:h-[56vh] tablet-sm:ml-12 min-[375px]:h-[92vh] tablet-md:h-[52vh] min-[390px]:h-[70vh] phone-md:max-h-[74vh] min-[428px]:h-[68vh] phone-lg:h-[72vh] py-8 ml-3">
+    <section className="relative phone-sm:h-[84vh] h-screen desktop-lg:ml-10 desktop-sm:h-[68vh] desktop-lg:h-[60vh] tablet-lg:h-[48vh] desktop-xl:h-[56vh] tablet-sm:ml-12 min-[375px]:h-[92vh] tablet-md:h-[52vh] min-[390px]:h-[70vh] phone-md:max-h-[74vh] min-[428px]:h-[68vh] phone-lg:h-[60vh] py-8 ml-3">
       <Divider
         widthPx="var(--hero-div-w)"
         heightPx="var(--block-div-sm-h)"
@@ -14,7 +14,7 @@ export const Description = () => {
       <h2 className="text-2xl text-grey-2 desktop-xl:text-5xl desktop-sm:text-4xl tablet-md:my-2 phone-lg:text-4xl tablet-md:text-5xl font-medium phone-sm:text-3xl">
         WHAT IS SHE LIKE
       </h2>
-      <p className="mt-2 desktop-xl:max-w-120 desktop-xl:text-xl desktop-sm:max-w-92 desktop-sm:text-base tablet-sm:text-lg tablet-md:max-w-116 tablet-sm:max-w-92 phone-md:max-w-80 text-xs phone-sm:text-sm phone-lg:pl-2 phone-md:text-base phone-sm:max-w-68 text-text leading-loose tracking-wide ml-3 w-full max-w-60 wrap-break-words whitespace-normal">
+      <p className="mt-2 desktop-xl:max-w-120 desktop-xl:text-xl desktop-sm:max-w-92 desktop-sm:text-base tablet-md:max-w-116 tablet-sm:max-w-92 phone-md:max-w-80 text-xs phone-sm:text-sm phone-lg:pl-2 phone-lg:max-w-76 phone-md:text-base phone-sm:max-w-68 text-text leading-loose tracking-wide ml-3 w-full max-w-60 wrap-break-words whitespace-normal">
         I'm a curious and disciplined developer who enjoys solving UI problems
         with clean, maintainable code. I prioritize performance and
         accessibility, and I'm committed to continuous learning. I work well in
@@ -29,7 +29,7 @@ export const Description = () => {
         height={200}
         style={{ position: "absolute", right: 0, bottom: 0 }}
         loading="lazy"
-        className="object-cover phone-md:w-36 desktop-xl:w-84 desktop-sm:w-72 desktop-sm:mr-24 phone-lg:w-40 tablet-sm:w-48 tablet-md:w-60"
+        className="object-cover phone-md:w-36 desktop-xl:w-84 desktop-sm:w-72 desktop-sm:mr-24 phone-lg:w-44 phone-lg:-mr-3 tablet-sm:w-52 tablet-md:w-60"
       />
     </section>
   );
