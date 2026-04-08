d589d35103c7e4a7c33f0876321b5d25c965ee5a
Alix Bell <x.bavayra@gmail.com>
2026-04-08 19:01:29 +0200

    style(Footer): changed the bg color to a global variable insted of hardcoded one; set my actual phone number

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
-    <footer className="py-3 text-center text-[0.75rem] max-w-52 mx-auto opacity-65 font-semibold border-t-2 border-accent-red text-grey-1 bg-[#4f4f4f] tablet-sm:text-sm">
+    <footer className="py-3 text-center text-[0.75rem] max-w-52 mx-auto opacity-65 font-semibold border-t-2 border-accent-red text-grey-1 bg-section-title-bg tablet-sm:text-sm">
       <div className="grid grid-cols-[1fr_max-content_1fr] tablet-sm:text-sm tablet-sm:max-w-64 desktop-xl:text-xl desktop-xl:max-w-[23rem]">
-        <a href="tel:+15550100000" aria-label="Call example number">
-          +1 (555) 010‑0000
+        <a href="tel:+420774640788" aria-label="Call example number">
+          +420 774 640 788
         </a>
         <span className="text-accent-red font-bold text-lg -mt-1 pl-1 tablet-sm:text-2xl">
           |
