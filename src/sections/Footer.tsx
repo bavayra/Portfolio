806c84e6bef64d4a13f2df487833595c7dc8b5b7
Alix Bell <x.bavayra@gmail.com>
2026-04-16 20:16:56 +0200

    refactor(Footer): removed excessive aria-label for phone number

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -2,9 +2,7 @@ const Footer = () => {
   return (
     <footer className="py-3 text-center text-[0.75rem] max-w-52 mx-auto opacity-65 font-semibold border-t-2 border-accent-red text-grey-1 bg-section-title-bg tablet-sm:text-sm">
       <div className="grid grid-cols-[1fr_max-content_1fr] tablet-sm:text-sm tablet-sm:max-w-64 desktop-xl:text-xl desktop-xl:max-w-[23rem]">
-        <a href="tel:+420774640788" aria-label="Phone number">
-          +420 774 640 788
-        </a>
+        <a href="tel:+420774640788">+420 774 640 788</a>
         <span className="text-accent-red font-bold text-lg -mt-1 pl-1 tablet-sm:text-2xl">
           |
         </span>
