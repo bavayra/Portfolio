4193718fb3e0f6ff1984ea3822b669b77031b571
Alix Bell <x.bavayra@gmail.com>
2026-04-17 20:09:57 +0200

    feat: removed phone number completely

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,13 +1,7 @@
 const Footer = () => {
   return (
     <footer className="py-3 text-center text-[0.75rem] max-w-52 mx-auto opacity-65 font-semibold border-t-2 border-accent-red text-grey-1 bg-section-title-bg tablet-sm:text-sm">
-      <div className="grid grid-cols-[1fr_max-content_1fr] tablet-sm:text-sm tablet-sm:max-w-64 desktop-xl:text-xl desktop-xl:max-w-[23rem]">
-        <a href="tel:+420774640788">+420 774 640 788</a>
-        <span className="text-accent-red font-bold text-lg -mt-1 pl-1 tablet-sm:text-2xl">
-          |
-        </span>
-        <p className="location">Prague, Czechia</p>
-      </div>
+      <p className="location">Prague, Czechia</p>
       <p className="tracking-wider leading-relaxed opacity-65 desktop-xl:text-lg">
         All Rights Reserved © {new Date().getFullYear()}
       </p>
