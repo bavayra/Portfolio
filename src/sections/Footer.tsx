aa647046eb2f1e907d2169a2a04fcd44329d88a9
Alix Bell <x.bavayra@gmail.com>
2026-04-17 20:47:17 +0200

    refactor(Footer): deleted location class (it doesn't exist?)

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="py-3 text-center text-[0.75rem] max-w-52 mx-auto opacity-65 font-semibold border-t-2 border-accent-red text-grey-1 bg-section-title-bg tablet-sm:text-sm">
-      <p className="location">Prague, Czechia</p>
+      <p>Prague, Czechia</p>
       <p className="tracking-wider leading-relaxed opacity-65 desktop-xl:text-lg">
         All Rights Reserved © {new Date().getFullYear()}
       </p>
