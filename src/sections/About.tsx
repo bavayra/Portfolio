c704801e4edcceba0d5e82f04e852ab2fbfce03e
Alix Bell <x.bavayra@gmail.com>
2026-04-20 20:08:04 +0200

    style(About): used pl instead of ml for the section. Increased border-r and paddings for the btn. Corrected font size clamp for the section's titles. Wrapped the page number and section title in div to set the correct leading. Set ring offset-bg

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -5,20 +5,22 @@ const About = () => {
       aria-labelledby="about-heading"
       className="relative py-8 ml-3 tablet-sm:ml-12 tablet-md:pb-0 desktop-lg:ml-10 desktop-xl:pb-12"
     >
-      <p
-        className="text-text text-(length:--font-page-number)"
-        aria-hidden="true"
-      >
-        01
-      </p>
-      <h3
-        className="text-(length:--font-section-label) text-grey-2 font-medium tablet-md:my-2 desktop-sm:text-4xl desktop-xl:text-5xl"
-        id="about-heading"
-      >
-        WHO AM I
-      </h3>
+      <div className="leading-6.5">
+        <p
+          className="text-text text-(length:--font-page-number)"
+          aria-hidden="true"
+        >
+          01
+        </p>
+        <h3
+          className="text-(length:--font-section-label) text-grey-2 font-medium tablet-md:my-2 desktop-sm:text-4xl desktop-xl:text-5xl"
+          id="about-heading"
+        >
+          WHO AM I
+        </h3>
+      </div>
 
-      <p className="mt-2 text-(length:--font-body-sm) text-text leading-loose tracking-wide ml-3 w-full wrap-break-word whitespace-normal phone-lg:pl-2 max-w-(--max-w-text-sm) desktop-sm:max-w-(--max-w-text-lg) desktop-xl:text-2xl border-l-2 border-grey-2">
+      <p className="mt-2 text-(length:--font-body-sm) text-text leading-loose tracking-wide w-full wrap-break-word whitespace-normal pl-2 max-w-(--max-w-text-sm) desktop-sm:max-w-(--max-w-text-lg) desktop-xl:text-2xl border-l-2 border-grey-2">
         I’m a junior front‑end developer focused on building polished,
         accessible interfaces with React and TypeScript. I care about
         performance and maintainability—using Vite and Tailwind to deliver fast,
@@ -31,9 +33,9 @@ const About = () => {
         href="/Alix_Bell_Frontend_Dev.pdf"
         download
         aria-label="Download CV (PDF)"
-        className="mt-2 inline-block mr-4 phone-sm:mt-4 phone-sm:mr-8 tablet-sm:mr-16 tablet-sm:mt-0 rounded-sm bg-accent-red text-white px-3 py-2 text-sm tablet-md:mr-30 desktop-sm:ml-6 desktop-sm:mt-4 focus:outline-none focus-visible:ring-2 focus-visible:ring-accent-red focus-visible:ring-offset-2 focus-visible:rounded-sm"
+        className="mt-2 inline-block mr-4 phone-sm:mt-4 phone-sm:mr-8 tablet-sm:mr-16 tablet-sm:mt-0 rounded-md bg-accent-red text-white px-4 py-2.5 text-sm tablet-md:mr-30 desktop-sm:ml-6 desktop-sm:mt-4 focus:outline-none focus-visible:ring-2 focus-visible:ring-accent-red focus-visible:ring-offset-2 focus-visible:rounded-md focus-visible:ring-offset-bg"
       >
-        Download CV <span className="text-xs opacity-70">(PDF)</span>
+        Download CV <span className="opacity-70">(PDF)</span>
       </a>
     </section>
   );
