4efad3b81c947e143a37fc3947d835573a249500
Alix Bell <x.bavayra@gmail.com>
2026-04-22 13:01:24 +0200

    style/refsctor(Header): fixed noncentered nav links positions with mx-auto. Deleted pr for the section and set correct paddings. Increased font size for home btn and set custoom paddings for it

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -21,7 +21,7 @@ const Header = () => {
 
   return (
     <header
-      className={`fixed top-0 text-sm desktop-xl:py-4 text-text font-medium left-0 w-full z-50 transition-transform duration-300 ${
+      className={`fixed top-0 text-sm desktop-xl:py-4 text-text font-medium w-full z-50 transition-transform duration-300 ${
         visible ? "translate-y-0" : "-translate-y-full"
       }`}
       inert={!visible || undefined}
@@ -29,15 +29,12 @@ const Header = () => {
       <div
         className={` flex items-center justify-end w-full ${visible ? "bg-bg/90 shadow-sm backdrop-blur" : "bg-transparent"} `}
       >
-        <nav
-          className="pr-4 phone-lg:py-2 desktop-sm:mx-auto"
-          aria-label="Main navigation"
-        >
+        <nav className="phone-lg:py-2 mx-auto" aria-label="Main navigation">
           <ul className="flex gap-0 desktop-sm:gap-x-4 ">
             <li className="hover:scale-110 active:scale-95 duration-300 hover:font-semibold">
               <a
                 href="#main-content"
-                className="inline-flex px-3 py-2 focus:outline-none text-accent-red font-bold focus-visible:ring-2 active:text-grey-3 focus-visible:ring-accent-red focus-visible:ring-offset-4 focus-visible:rounded-lg focus-visible:ring-offset-bg hover:text-grey-3 "
+                className="inline-flex py-2.5 px-3 text-base focus:outline-none text-accent-red font-bold focus-visible:ring-2 active:text-grey-3 focus-visible:ring-accent-red focus-visible:ring-offset-4 focus-visible:rounded-lg focus-visible:ring-offset-bg hover:text-grey-3 "
               >
                 HOME
               </a>
@@ -45,7 +42,7 @@ const Header = () => {
             <li className="hover:text-accent-red hover:scale-110 active:scale-95 duration-300 hover:font-semibold">
               <a
                 href="#about-section"
-                className="inline-flex px-3 py-2 focus:outline-none focus-visible:ring-2 active:text-grey-3  focus-visible:ring-accent-red focus-visible:ring-offset-4 focus-visible:rounded-lg focus-visible:ring-offset-bg"
+                className="inline-flex p-3 focus:outline-none focus-visible:ring-2 active:text-grey-3  focus-visible:ring-accent-red focus-visible:ring-offset-4 focus-visible:rounded-lg focus-visible:ring-offset-bg"
               >
                 ABOUT
               </a>
@@ -53,7 +50,7 @@ const Header = () => {
             <li className="hover:text-accent-red hover:scale-110 active:scale-95 duration-300 hover:font-semibold">
               <a
                 href="#projects-section"
-                className="inline-flex px-3 py-2 focus:outline-none focus-visible:ring-2 active:text-grey-3  focus-visible:ring-accent-red focus-visible:ring-offset-4 focus-visible:rounded-lg focus-visible:ring-offset-bg"
+                className="inline-flex p-3 focus:outline-none focus-visible:ring-2 active:text-grey-3  focus-visible:ring-accent-red focus-visible:ring-offset-4 focus-visible:rounded-lg focus-visible:ring-offset-bg"
               >
                 PROJECTS
               </a>
@@ -61,7 +58,7 @@ const Header = () => {
             <li className="hover:text-accent-red hover:scale-110  active:scale-95 duration-300 hover:font-semibold">
               <a
                 href="#contacts-section"
-                className="inline-flex px-3 py-2 focus:outline-none focus-visible:ring-2 active:text-grey-3 focus-visible:ring-accent-red focus-visible:ring-offset-4 focus-visible:rounded-lg focus-visible:ring-offset-bg"
+                className="inline-flex p-3 focus:outline-none focus-visible:ring-2 active:text-grey-3 focus-visible:ring-accent-red focus-visible:ring-offset-4 focus-visible:rounded-lg focus-visible:ring-offset-bg"
               >
                 CONTACTS
               </a>
