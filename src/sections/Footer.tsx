651a8f142991d887f8d1dbc40fb80a9458999594
Alix Bell <x.bavayra@gmail.com>
2026-04-22 17:26:27 +0200

    style(Footer): corrected footer's bg color and deleted opacity property

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="py-3 text-center text-[0.85rem] max-w-full opacity-65 font-semibold text-grey-1 bg-section-title-bg tablet-sm:text-sm">
+    <footer className="py-3 text-center text-[0.85rem] max-w-full font-semibold text-grey-1 bg-section-title-bg tablet-sm:text-sm">
       <p>Prague, Czechia</p>
       <p className="tracking-wider leading-relaxed opacity-65 desktop-xl:text-lg">
         All Rights Reserved © {new Date().getFullYear()}
