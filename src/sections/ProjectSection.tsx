66b6a4a9e0040427898c48b967aa129e1fb95155
Alix Bell <x.bavayra@gmail.com>
2026-04-22 13:21:07 +0200

    style(ProjectSection): wrapped shorst description and exteranl links in div and moved border there so it will contain both elements

--- src/sections/ProjectSection.tsx
+++ src/sections/ProjectSection.tsx
@@ -43,15 +43,16 @@ const ProjectSection = ({
           >
             {project.title}
           </h3>
-
-          <p className="mt-2 text-(length:--font-body-sm) border-l-2 border-grey-2 pl-3 text-text leading-loose tracking-wide w-full wrap-break-word whitespace-normal max-w-(--max-w-text-sm) desktop-sm:max-w-(--max-w-text-lg) desktop-xl:text-2xl">
-            {project.descriptionShort}
-          </p>
-          <ExternalProjectLink
-            liveUrl={project.liveUrl}
-            githubUrl={project.githubUrl}
-            projectTitle={project.title}
-          />
+          <div className=" border-l-2 border-grey-2">
+            <p className="mt-2 text-(length:--font-body-sm) pl-3 text-text leading-loose tracking-wide w-full wrap-break-word whitespace-normal max-w-(--max-w-text-sm) desktop-sm:max-w-(--max-w-text-lg) desktop-xl:text-2xl">
+              {project.descriptionShort}
+            </p>
+            <ExternalProjectLink
+              liveUrl={project.liveUrl}
+              githubUrl={project.githubUrl}
+              projectTitle={project.title}
+            />
+          </div>
         </div>
 
         <div
