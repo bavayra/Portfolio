56be7390a0a9aa64727778dfc2e6b05a62bcf178
Alix Bell <x.bavayra@gmail.com>
2026-04-26 16:34:06 +0200

    style(hero-1024): translated div wrapper for text elements (y). Corrected top positioning and height of dividers, set left position for them. Changed dotPattern sizing increased from desktop-md to desktop-sm. Increased circles' sizes. Increased leading for the name element and corrected its position

--- src/components/DotPattern.tsx
+++ src/components/DotPattern.tsx
@@ -3,13 +3,13 @@ import { useId } from "react";
 const DotPattern = () => {
   const uid = useId();
   return (
-    <div className="absolute bottom-0 desktop-sm:translate-x-0.5 inset-x-0 w-screen left-1/2 -translate-x-1/2 tablet-sm:translate-y-[12%] tablet-md:translate-y-[24%]">
+    <div className="absolute bottom-0 desktop-sm:translate-x-0.5 inset-x-0 w-screen left-1/2 -translate-x-1/2 tablet-sm:translate-y-[12%] tablet-md:translate-y-[24%] desktop-sm:translate-x-[18%]">
       <svg
         id={`${uid}-svg-sm`}
         width="100%"
         height="100%"
         viewBox="0 0 180 180"
-        className="tablet-sm:pl-8 w-full h-full pl-5 block desktop-lg:hidden"
+        className="tablet-sm:pl-8 w-full h-full pl-5 block desktop-sm:hidden"
       >
         <defs>
           <pattern
@@ -29,14 +29,14 @@ const DotPattern = () => {
         width="100%"
         height="100%"
         viewBox="0 0 320 320"
-        className="w-full h-full desktop-lg:block hidden desktop-lg:w-[calc(100%+48px)]"
+        className="w-full h-full hidden desktop-sm:block desktop-sm:w-[calc(100%+48px)]"
       >
         <defs>
           <pattern
             id={`${uid}-dot-xl`}
             patternUnits="userSpaceOnUse"
-            width={16}
-            height={16}
+            width={18}
+            height={18}
           >
             <circle cx="3" cy="3" fill="var(--color-accent-red)" r={3} />
           </pattern>
