cdf79cd84b62a78f049c405a70ecc8bd2dc3f803
Alix Bell <x.bavayra@gmail.com>
2026-04-27 18:24:19 +0200

    style(hero-1280): decreased ml for the top divider and text elements. Decreased pl for the name element. Decreased translate-x for dotPattern

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -14,12 +14,12 @@ const Hero = () => {
       deliberate trade-off: the h1 gives screen readers and search engines both
       the name and the role together, while the visual design keeps them split
       across the layout. */}
-      <div className="hidden desktop-sm:block tablet-sm:ml-8 desktop-md:ml-10 relative">
-        <div className="absolute left-5 top-0 min-h-38 w-(--hero-div-w) bg-grey-2" />
+      <div className="hidden desktop-sm:block tablet-sm:ml-8 desktop-md:ml-8 relative">
+        <div className="absolute left-5 top-0 min-h-42 w-(--hero-div-w) bg-grey-2" />
       </div>
 
       <div
-        className="grid grid-cols-[auto_1fr] grid-rows-[auto_auto_auto] ml-3 tablet-sm:ml-8 desktop-sm:translate-y-[70%] desktop-md:ml-10 relative"
+        className="grid grid-cols-[auto_1fr] grid-rows-[auto_auto_auto] ml-3 tablet-sm:ml-8 desktop-sm:translate-y-[70%] desktop-md:ml-8 relative"
         aria-hidden="true"
       >
         <p className="col-start-1 row-start-1 pl-3 text-(length:--name-year-size) text-grey-2 leading-8 phone-md:leading-10 phone-lg:leading-12 desktop-md:leading-16">
@@ -30,11 +30,11 @@ const Hero = () => {
           Portfolio
         </p>
 
-        <p className="col-start-1 col-span-2 row-start-3 pl-[40%] tablet-sm:pl-[49%] tablet-md:pl-[36%] tablet-lg:pl-[45%] desktop-sm:pl-[34%] desktop-md:pl-[35%] desktop-lg:pl-[30%] text-(length:--name-year-size) font-bold text-accent-red tracking-wider tablet-lg:text-[3.5rem] desktop-xl:text-7xl leading-10 phone-md:leading-12 tablet-sm:leading-14 desktop-sm:leading-18 desktop-lg:leading-20">
+        <p className="col-start-1 col-span-2 row-start-3 pl-[40%] tablet-sm:pl-[49%] tablet-md:pl-[36%] tablet-lg:pl-[45%] desktop-sm:pl-[34%] desktop-md:pl-[29%] desktop-lg:pl-[30%] text-(length:--name-year-size) font-bold text-accent-red tracking-wider tablet-lg:text-[3.5rem] desktop-xl:text-7xl leading-10 phone-md:leading-12 tablet-sm:leading-14 desktop-sm:leading-18 desktop-lg:leading-20">
           ALIX BELL
         </p>
       </div>
-      <div className="absolute left-13 tablet-lg:left-14 tablet-lg:top-46 top-40 min-h-[25vh] tablet-lg:min-h-[28vh] desktop-sm:top-84 desktop-sm:min-h-86 desktop-sm:left-13 w-(--hero-div-w) bg-grey-2" />
+      <div className="absolute left-13 tablet-lg:left-14 tablet-lg:top-46 top-40 min-h-[25vh] tablet-lg:min-h-[28vh] desktop-sm:top-84 desktop-md:top-90  desktop-sm:min-h-86 desktop-sm:left-13 w-(--hero-div-w) bg-grey-2" />
       <div className="mt-auto">
         <DotPattern />
       </div>
