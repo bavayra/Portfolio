be4916b6be5ed71f71c453274f28ca5a2082dea4
Alix Bell <x.bavayra@gmail.com>
2026-04-30 19:40:26 +0200

    style(ProjectScreen-1728): decreased screens' and their container's  w. Removed mt from the dogShelter description. Removed duplicated classes. Corrected translates for fullDescriptions. Deacreased w of the fullDescription containers. Increased mb for the DogShelter

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -33,10 +33,10 @@ export const projects = [
     liveUrl: "https://bavayra.github.io/WeatherApp/",
     githubUrl: "https://github.com/bavayra/WeatherApp.git",
     imageClasses: {
-      0: "mx-auto phone-md:mt-2 phone-md:w-36 phone-lg:-mt-5 phone-lg:w-44 tablet-sm:w-52 tablet-md:w-60 desktop-sm:w-40 desktop-xl:w-52 desktop-sm:mt-0",
-      1: "-mt-6 mx-auto phone-md:w-36 phone-lg:-mt-14 phone-lg:w-44 tablet-sm:w-52 tablet-md:w-60 desktop-sm:w-40 desktop-xl:w-52 desktop-sm:mt-0",
-      2: "mx-auto phone-md:w-34 phone-lg:w-44 tablet-sm:mt-2 tablet-sm:w-52 tablet-md:w-60 desktop-sm:w-40 desktop-xl:w-52 desktop-sm:mt-0",
-      3: "mx-auto phone-md:-mt-3 phone-md:w-36 phone-lg:-mt-2 phone-lg:w-44 tablet-sm:-mt-2 tablet-sm:w-52 tablet-md:w-60 desktop-sm:w-40 desktop-xl:w-52 desktop-sm:mt-0",
+      0: "mx-auto phone-md:mt-2 phone-md:w-36 phone-lg:-mt-5 phone-lg:w-44 tablet-sm:w-52 tablet-md:w-60 desktop-sm:w-40 desktop-xl:w-44 desktop-sm:mt-0",
+      1: "-mt-6 mx-auto phone-md:w-36 phone-lg:-mt-14 phone-lg:w-44 tablet-sm:w-52 tablet-md:w-60 desktop-sm:w-40 desktop-xl:w-44 desktop-sm:mt-0",
+      2: "mx-auto phone-md:w-34 phone-lg:w-44 tablet-sm:mt-2 tablet-sm:w-52 tablet-md:w-60 desktop-sm:w-40 desktop-xl:w-44 desktop-sm:mt-0",
+      3: "mx-auto phone-md:-mt-3 phone-md:w-36 phone-lg:-mt-2 phone-lg:w-44 tablet-sm:-mt-2 tablet-sm:w-52 tablet-md:w-60 desktop-sm:w-40 desktop-xl:w-44 desktop-sm:mt-0",
     },
     images: [
       {
@@ -81,10 +81,10 @@ export const projects = [
     githubUrl: "https://github.com/bavayra/dog-shelter.git",
     liveUrl: "https://dogshelter-project.netlify.app/",
     imageClasses: {
-      0: "mx-auto phone-md:mt-3 phone-md:w-36 phone-lg:-mt-3 phone-lg:w-44 tablet-sm:w-52 tablet-md:w-60 desktop-sm:w-40 desktop-sm:mt-0 desktop-xl:w-52",
-      1: "-mt-24 mx-auto phone-md:-mt-25 phone-md:w-36 phone-lg:-mt-36 phone-lg:w-44 tablet-sm:-mt-42 tablet-sm:w-52 tablet-md:w-60 desktop-sm:w-40 desktop-sm:mt-0 desktop-xl:w-52",
-      2: "mx-auto phone-md:w-38 phone-lg:w-44 tablet-sm:w-52 tablet-md:w-60 desktop-sm:w-40 desktop-sm:mt-5 desktop-xl:w-52 desktop-md:translate-y-1/2",
-      3: "-mt-17 mx-auto phone-md:-mt-22 phone-md:w-36 phone-lg:-mt-24 phone-lg:w-44 tablet-sm:-mt-30 tablet-sm:w-48 tablet-md:-mt-32 tablet-md:w-60 desktop-sm:w-40 desktop-sm:-mt-12 desktop-md:-translate-y-[5%] desktop-xl:w-52",
+      0: "mx-auto phone-md:mt-3 phone-md:w-36 phone-lg:-mt-3 phone-lg:w-44 tablet-sm:w-52 tablet-md:w-60 desktop-sm:w-40 desktop-sm:mt-0 desktop-xl:w-44",
+      1: "-mt-24 mx-auto phone-md:-mt-25 phone-md:w-36 phone-lg:-mt-36 phone-lg:w-44 tablet-sm:-mt-42 tablet-sm:w-52 tablet-md:w-60 desktop-sm:w-40 desktop-sm:mt-0 desktop-xl:w-44",
+      2: "mx-auto phone-md:w-38 phone-lg:w-44 tablet-sm:w-52 tablet-md:w-60 desktop-sm:w-40 desktop-sm:mt-5 desktop-xl:w-44 desktop-md:translate-y-1/2",
+      3: "-mt-17 mx-auto phone-md:-mt-22 phone-md:w-36 phone-lg:-mt-24 phone-lg:w-44 tablet-sm:-mt-30 tablet-sm:w-48 tablet-md:-mt-32 tablet-md:w-60 desktop-sm:w-40 desktop-sm:-mt-12 desktop-md:-translate-y-[5%] desktop-xl:w-44",
     },
     images: [
       {
@@ -130,10 +130,10 @@ export const projects = [
     githubUrl: "https://github.com/bavayra/portfolio.git",
     liveUrl: "https://alix-bell-portfolio.netlify.app",
     imageClasses: {
-      0: "mx-auto -mt-13 w-34 phone-md:w-36 phone-md:-mt-10 phone-lg:w-44 phone-lg:-mt-22 tablet-sm:w-52 tablet-md:-mt-34 tablet-md:w-60 border-2 border-grey-2 desktop-sm:w-44 desktop-sm:mt-0 desktop-xl:w-52",
-      1: "-mt-4 mx-auto w-34 phone-md:-mt-3 phone-md:w-36 phone-lg:-mt-12 phone-lg:w-44 tablet-sm:-mt-14 tablet-sm:w-50 tablet-md:-mt-26 tablet-md:w-60 border-2 border-grey-2 desktop-sm:w-44 desktop-xl:w-52 desktop-sm:mt-0 ",
-      2: "-mt-12 mx-auto w-30 phone-md:-mt-13 phone-md:w-36 phone-lg:w-44 phone-lg:-mt-16 tablet-sm:-mt-15 tablet-sm:w-50 tablet-md:-mt-26 tablet-md:w-60 border-2 border-grey-2 desktop-sm:w-44 desktop-xl:w-52 desktop-sm:-mt-20  desktop-md:-translate-y-[5%]",
-      3: "mx-auto w-30 phone-md:w-36 phone-lg:w-44 tablet-sm:mt-3 tablet-sm:w-54 tablet-md:w-60 border-2 border-grey-2 desktop-sm:w-44 desktop-xl:w-52 desktop-sm:mt-4 ",
+      0: "mx-auto -mt-13 w-34 phone-md:w-36 phone-md:-mt-10 phone-lg:w-44 phone-lg:-mt-22 tablet-sm:w-52 tablet-md:-mt-34 tablet-md:w-60 border-2 border-grey-2 desktop-sm:w-44 desktop-sm:mt-0 desktop-xl:w-44",
+      1: "-mt-4 mx-auto w-34 phone-md:-mt-3 phone-md:w-36 phone-lg:-mt-12 phone-lg:w-44 tablet-sm:-mt-14 tablet-sm:w-50 tablet-md:-mt-26 tablet-md:w-60 border-2 border-grey-2 desktop-sm:w-44 desktop-xl:w-44 desktop-sm:mt-0 ",
+      2: "-mt-12 mx-auto w-30 phone-md:-mt-13 phone-md:w-36 phone-lg:w-44 phone-lg:-mt-16 tablet-sm:-mt-15 tablet-sm:w-50 tablet-md:-mt-26 tablet-md:w-60 border-2 border-grey-2 desktop-sm:w-44 desktop-xl:w-44 desktop-sm:-mt-20  desktop-md:-translate-y-[5%]",
+      3: "mx-auto w-30 phone-md:w-36 phone-lg:w-44 tablet-sm:mt-3 tablet-sm:w-54 tablet-md:w-60 border-2 border-grey-2 desktop-sm:w-44 desktop-xl:w-44 desktop-sm:mt-4 ",
     },
     images: [
       {
