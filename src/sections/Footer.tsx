20251f8e5072f3c4235e338b5c453f7290bdaa78
Alix Bell <x.bavayra@gmail.com>
2026-04-30 19:48:41 +0200

    style(Footer-1728): decreased font sizes for text elements

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -2,10 +2,10 @@ const Footer = () => {
   return (
     <footer className="py-3 text-center text-[0.85rem] max-w-full font-semibold text-grey-1 bg-section-title-bg tablet-sm:text-sm">
       <p>Prague, Czechia</p>
-      <p className="tracking-wider leading-relaxed opacity-65 desktop-xl:text-lg">
+      <p className="tracking-wider leading-relaxed opacity-65 desktop-xl:text-base">
         All Rights Reserved © {new Date().getFullYear()}
       </p>
-      <p className="tracking-wider opacity-65 desktop-xl:text-lg">
+      <p className="tracking-wider opacity-65 desktop-xl:text-base">
         Created by Alix Bell with ❤️
       </p>
     </footer>
